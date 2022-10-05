0f54dc2736daac9c9cb7110126d2b5cad7bc710c
Jodi Kusuma <jodikusuma2013@gmail.com>
2022-10-05 13:00:17 +0700

    Adding Client

---
+++ DTSMCC_Client/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DTSMCC_Client.Controllers
+{
+    public class AccountController : Controller
+    {
+        HttpClient HttpClient;
+        string address;
+        public AccountController()
+        {
+            this.address = "https://localhost:44376/index.html";
+            HttpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address)
+            };
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login login)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(login), encoding.UTF8, "application/json");
+            var result = HttpClient.PostAsync(address, content).Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var data = JsonConvert.DeserializeObject<ResponseClient>(await result.Content.ReadAsStringAsync());
+                HttpContext.Session.SetString("Role", data.data.Role);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
